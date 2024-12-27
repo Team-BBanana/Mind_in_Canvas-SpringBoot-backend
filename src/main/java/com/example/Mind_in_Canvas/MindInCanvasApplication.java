2314d9908adccdd810f499c32b07bca3c04c0cb1
hongyongjae <developsvai5096@gmail.com>
2024-12-27 22:37:16 +0900

    imitial commit

---
+++ src/main/java/com/example/Mind_in_Canvas/MindInCanvasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mind_in_Canvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindInCanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindInCanvasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mind_in_Canvas/MindInCanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mind_in_Canvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindInCanvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
